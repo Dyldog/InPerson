2d300be1f72bdcc786baedfba6bc281e6c5dc6bf
Harry Singh <hhs4harry@gmail.com>
2022-11-26 14:28:36 +1100

    Add Config

---
+++ Source/InPerson/inperson/Config/Config.swift
@@ -0,0 +1,15 @@
+//
+//  Config.swift
+//  inperson
+//
+//  Created by Harry Singh on 26/11/2022.
+//
+
+import Foundation
+
+enum Config {
+    static let teamID: String = ""
+    static let cert: String = ""
+    static let authKeyID: String = ""
+    static let topic: String = ""
+}
